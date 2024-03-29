d7d65c1832de435563685d3a53939910a277490f
othman93290 <7566@holbertonstudents.com>
2024-03-29 21:53:01 +0100

    .\models\amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""This module defines a class Amenity"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
+""" State Module for HBNB project """
 from sqlalchemy.orm import relationship
+from models.base_model import Base, BaseModel
+from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
-    """Defines attributes for the Amenity class"""
-    __tablename__ = "amenities"
+    """
+    Amenity inherits from BaseModel and Base (respect the order)
+    """
+    __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    place_amenities = relationship(
-        "Place", secondary="place_amenity", viewonly=False)
+    places_amenities = relationship(
+        "Place",
+        secondary="place_amenity")
