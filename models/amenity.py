f4ad172b2d6c7d192540ef2fcf44ebf851dbf33c
othman93290 <7566@holbertonstudents.com>
2024-03-29 21:56:59 +0100

    pull

--- models/amenity.py
+++ models/amenity.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, ForeignKey
 
 
 class Amenity(BaseModel, Base):
-    """
-    Amenity inherits from BaseModel and Base (respect the order)
-    """
+    """Amenity representation"""
     __tablename__ = 'amenities'
     name = Column(String(128), nullable=False)
-    places_amenities = relationship(
-        "Place",
-        secondary="place_amenity")
+    place_amenities = relationship(
+        "Place", secondary="place_amenity", viewonly=True)
