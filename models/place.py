d786b812b32224d73a4c72e789209cd20bc55d1c
othman93290 <7566@holbertonstudents.com>
2024-03-29 21:54:17 +0100

    test

--- models/place.py
+++ models/place.py
@@ -1,32 +1,37 @@
-#!/usr/bin/python3
-"""This module defines a class Place"""
-import models
+#!/usr/bin/python
+""" holds class Place"""
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.sql.schema import Table
+from sqlalchemy.sql.sqltypes import Float, Integer
 from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 from models.review import Review
-from models.amenity import Amenity
-from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
-from sqlalchemy.orm import relationship
+import models
 from os import getenv
 
-
-"""Create a table for the relationship between Place and Amenity"""
-metadata = Base.metadata
-place_amenity = Table("place_amenity", metadata,
-                      Column("place_id", String(60),
-                             ForeignKey("places.id"),
-                             primary_key=True,
-                             nullable=False),
-                      Column("amenity_id", String(60),
-                             ForeignKey("amenities.id"),
-                             primary_key=True,
-                             nullable=False))
+place_amenity = Table(
+    'place_amenity', Base.metadata,
+    Column(
+        'place_id',
+        String(60),
+        ForeignKey('places.id'),
+        primary_key=True,
+        nullable=False),
+    Column(
+        'amenity_id',
+        String(60),
+        ForeignKey('amenities.id'),
+        primary_key=True,
+        nullable=False)
+)
 
 
 class Place(BaseModel, Base):
-    """Define new attributes for the Place class"""
-    __tablename__ = "places"
-    city_id = Column(String(60), ForeignKey("cities.id"), nullable=False)
-    user_id = Column(String(60), ForeignKey("users.id"), nullable=False)
+    """ A place to stay """
+
+    __tablename__ = 'places'
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024))
     number_rooms = Column(Integer, nullable=False, default=0)
@@ -36,32 +41,29 @@ class Place(BaseModel, Base):
     latitude = Column(Float)
     longitude = Column(Float)
     amenity_ids = []
-    reviews = relationship("Review", cascade="delete", backref="place")
-    amenities = relationship(
-        "Amenity", secondary="place_amenity",
-        viewonly=False, back_populates="place_amenities")
 
-    if getenv("HBNB_TYPE_STORAGE") != "db":
-        @property
-        def reviews(self):
-            """Method that returns the list of Review instances"""
-            reviews_list = []
-            for review_obj in models.storage.all(Review).values():
-                if self.id == review_obj.place_id:
-                    reviews_list.append(review_obj)
-            return reviews_list
+    reviews = relationship(
+        'Review',
+        backref='state',
+        cascade="all, delete, delete-orphan"
+    )
 
-        @property
-        def amenities(self):
-            """Method that returns the list of Amenity instances"""
-            amenities_list = []
-            for amenity_obj in models.storage.all(Amenity).values():
-                if amenity_obj.id in amenities_list:
-                    amenities_list.append(amenity_obj)
-            return amenities_list
+    @property
+    def reviews(self):
+        """ Getter that that returns the list of Reviews instances """
+        instances = models.storage.all(Review)
+        new = []
+        for review in instances.values():
+            if review.place_id == (self.id):
+                new.append(review)
+        return new
 
-        @amenities.setter
-        def amenities(self, value):
-            """Method that handles append method for adding an Amenity.id"""
-            if value.__class__ == Amenity:
-                self.amenity_ids.append(value.id)
+    @reviews.setter
+    def amenities(self, obj):
+        """
+        Setter attribute amenities that handles append method
+        for adding an Amenity.id to the attribute amenity_ids.
+        """
+        from models.amenity import Amenity
+        if isinstance(obj, Amenity):
+            self.amenity_ids.append(obj.id)
