9a8b732f5406a793842f9e6f6189feb3270e8f12
othman93290 <7566@holbertonstudents.com>
2024-04-12 20:01:28 +0200

    db_storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -61,3 +61,8 @@ class DBStorage:
             bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
         self.__session = Session()
+
+    def close(self):
+        """Method to close the current session"""
+        self.reload()
+        self.__session.close()
